ca0afdfe95a0b85e1daf18c87ec028bc9aa115e8
mgsykora <mgsykora@gmail.com>
2020-01-07 01:56:53 +0000

    End2end samples

---
+++ startMinimalNotebookDocker.sh
@@ -0,0 +1,1 @@
+docker run -it -v /c/Users/Marti/Documents/tf2Samples:/home/jovyan/tf2Samples -p 28888:8888 -e GRANT_SUDO=yes --user root jupyter/minimal-notebook
---
+++ startTensorboardNotebookDocker.sh
@@ -0,0 +1,1 @@
+docker run -it -v /c/Users/Marti/Documents:/home/jovyan/dslab -p 28888:8888 -e GRANT_SUDO=yes --user root lspvic/tensorboard-notebook
